8a2aa3980224116011b0a62076f5f571b74a292d
Dmitrii Malyshev <malyshevds88@yandex.ru>
2022-12-08 10:54:02 +0800

    fix: comment reviewer 2

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true }, (err) => {
-  const message = err || 'mongdb is connected';
-  console.log(message);
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', { useNewUrlParser: true });
+// TODO(immotal) Useful for debugging, in the future I will change it to a log file
+// , (err) => {
+//   const message = err || 'mongdb is connected';
+//   console.log(message);
+// }
 
 app.use(indexRouter);
 
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
+// TODO(immotal) Useful for debugging, in the future I will change it to a log file
+// , () => {
+//   console.log(`App listening on port ${PORT}`);
+// }
--- models/user.js
+++ models/user.js
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function find(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
